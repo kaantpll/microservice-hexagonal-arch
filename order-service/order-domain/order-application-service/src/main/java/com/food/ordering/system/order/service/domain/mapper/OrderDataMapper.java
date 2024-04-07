7a39ec63875768f8a16a0fcc76dba3958f1caf18
kaan topal <kaant43@gmail.com>
2024-04-07 14:10:31 +0300

    creating of order service helper

--- order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/mapper/OrderDataMapper.java
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/mapper/OrderDataMapper.java
@@ -7,6 +7,7 @@ import com.food.ordering.system.domain.entity.valueobject.RestaurantId;
 import com.food.ordering.system.order.service.domain.dto.create.CreateOrderCommand;
 import com.food.ordering.system.order.service.domain.dto.create.CreateOrderResponse;
 import com.food.ordering.system.order.service.domain.dto.create.OrderAddress;
+import com.food.ordering.system.order.service.domain.dto.track.TrackOrderResponse;
 import com.food.ordering.system.order.service.domain.entity.Order;
 import com.food.ordering.system.order.service.domain.entity.Product;
 import com.food.ordering.system.order.service.domain.entity.Restaurant;
@@ -39,10 +40,19 @@ public class OrderDataMapper {
                 .build();
     }
 
-    public CreateOrderResponse orderToCreateOrderResponse(Order order){
+    public CreateOrderResponse orderToCreateOrderResponse(Order order,String message){
         return CreateOrderResponse.builder()
                 .orderTrackingId(order.getTrackingId().getValue())
                 .orderStatus(order.getOrderStatus())
+                .message(message)
+                .build();
+    }
+
+    public TrackOrderResponse orderToTrackOrderResponse(Order order){
+        return TrackOrderResponse.builder().
+                orderTrackingId(order.getTrackingId().getValue())
+                .orderStatus(order.getOrderStatus())
+                .failureMessages(order.getFailureMessages())
                 .build();
     }
 
---
+++ order-service/order-domain/order-application-service/src/test/java/com/food/ordering/system/order/service/domain/OrderTestConfiguration.java
@@ -0,0 +1,33 @@
+package com.food.ordering.system.order.service.domain;
+
+
+import com.food.ordering.system.order.service.domain.ports.output.repository.*;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication(scanBasePackages = "com.food.ordering.system")
+public class OrderTestConfiguration {
+
+    @Bean
+    public OrderRepository orderRepository() {
+        return Mockito.mock(OrderRepository.class);
+    }
+
+    @Bean
+    public CustomerRepository customerRepository() {
+        return Mockito.mock(CustomerRepository.class);
+    }
+
+    @Bean
+    public RestaurantRepository restaurantRepository() {
+        return Mockito.mock(RestaurantRepository.class);
+    }
+
+    @Bean
+    public OrderDomainService orderDomainService() {
+        return new OrderDomainServiceImpl();
+    }
+
+}
